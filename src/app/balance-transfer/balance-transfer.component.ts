218c97a293e0c801cb0e7489034a96b9ef331ace
p.saikarthik@gmail.com <p.saikarthik@gmail.com>
2020-02-25 11:35:26 -0500

    latest code

--- src/app/balance-transfer/balance-transfer.component.ts
+++ src/app/balance-transfer/balance-transfer.component.ts
@@ -13,22 +13,39 @@ export class BalanceTransferComponent implements OnInit {
   amount: number;
   HarshaCurrentbal: number;
   ScottCurrentbal: number;
+  transaction = [];
   constructor() { }
 
+
   ngOnInit() {
+    this.transaction = [];
   }
 
   add() {
     if (this.fromAcc !== this.toAcc && this.amount > 0 && this.amount < 10000) {
       console.log(`adding balances`);
+      this.transaction.push({ From: this.fromAcc, TO: this.toAcc, Amount: this.amount });
     } else {
       // show error
       console.log(`To account and from account cannot be same or amount is invalid`);
     }
   }
-
   calculate() {
     console.log(`calculating balances`);
-  }
 
+    this.transaction.forEach((transfer: any) => {
+      if (transfer.From === 'Scott') {
+        this.ScottCurrentbal = this.ScottCurrentbal - transfer.Amount;
+      }
+    });
+  }
+  balancetransaction(input: number, username: string) {
+    if (username === 'harsha') {
+      this.HarshaCurrentbal = input;
+    } else {
+      this.ScottCurrentbal = input;
+
+    }
+
+  }
 }
